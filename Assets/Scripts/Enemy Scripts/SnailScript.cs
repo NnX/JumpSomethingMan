7984163f79d7cf7018382453d446db8e5abf1642
NnX <hentis@ukr.net>
2020-06-11 11:49:48 +0300

    Implemented player movement and animation, implemented snail movement and animation, implemented collision detection

---
+++ Assets/Scripts/Enemy Scripts/SnailScript.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnailScript : MonoBehaviour
+{
+    public float moveSpeed = 1f;
+    Rigidbody2D snailBody;
+    Animator anim;
+    public LayerMask playerLayer;
+    bool isMoveLeft;
+
+    bool isCanMove;
+    bool isStunned;
+    public Transform left_Collision, right_Collision, top_Collision, down_Collision;
+    Vector3 left_Collision_Position, right_Collision_Position;
+
+    private void Awake()
+    {
+        snailBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+        left_Collision_Position = left_Collision.position;
+        right_Collision_Position = right_Collision.position;
+    }
+    void Start()
+    {
+        isMoveLeft = true;
+        isCanMove = true;
+    }
+
+    void Update()
+    {
+        //print("[Update] isCanMove  =" + isCanMove);
+        if (isCanMove)
+        {
+            if(!isStunned)
+            {
+                if(isMoveLeft)
+                {
+                    //print("move left");
+                    snailBody.velocity = new Vector2(-moveSpeed, snailBody.velocity.y);
+                } else
+                {
+                    snailBody.velocity = new Vector2(moveSpeed, snailBody.velocity.y);
+                    //print("move right");
+                }
+
+            }
+        }
+        CheckCollision();
+    }
+
+    void CheckCollision()
+    {
+        RaycastHit2D leftHit = Physics2D.Raycast(left_Collision.position, Vector2.left, 0.1f, playerLayer);
+        RaycastHit2D rightHit = Physics2D.Raycast(right_Collision.position, Vector2.right, 0.1f, playerLayer);
+
+        Collider2D topHit = Physics2D.OverlapCircle(top_Collision.position, 0.2f, playerLayer);
+
+        if (topHit != null)
+        {
+            if(topHit.gameObject.tag == MyTags.PLAYER_TAG)
+            {
+                if (!isStunned)
+                {
+                    topHit.gameObject.GetComponent<Rigidbody2D>().velocity = 
+                        new Vector2(topHit.gameObject.GetComponent<Rigidbody2D>().velocity.x, 7f);
+                    isCanMove = false;
+                    snailBody.velocity = new Vector2(0,0);
+                    anim.Play("Stunned");
+                    isStunned = true;
+                }
+            }
+        }
+
+        if(leftHit)
+        {
+            if(leftHit.collider.gameObject.tag == MyTags.PLAYER_TAG)
+            {
+                if(!isStunned)
+                {
+                    //TODO kill player
+                    print("Damage left");
+                } else
+                {
+                    snailBody.velocity = new Vector2(15f, snailBody.velocity.y);
+                }
+            }
+        } else if(rightHit)
+        {
+            if(rightHit.collider.gameObject.tag == MyTags.PLAYER_TAG)
+            {
+                if(!isStunned)
+                {
+                    // TODO kill player
+                    print("Damage right");
+                } else
+                {
+                    snailBody.velocity = new Vector2(-15f, snailBody.velocity.y);
+                }
+            }
+        }
+        if (!Physics2D.Raycast(down_Collision.position, Vector2.down, 0.1f))
+        {
+            ChangeDirection();
+        }
+
+    }
+
+    void ChangeDirection()
+    {
+        isMoveLeft = !isMoveLeft;
+
+        Vector3 tempScale = transform.localScale;
+
+        if(isMoveLeft)
+        {
+            tempScale.x = Mathf.Abs(tempScale.x);
+            left_Collision.position = left_Collision_Position;
+            right_Collision.position = right_Collision_Position;
+        } else
+        {
+            tempScale.x = -Mathf.Abs(tempScale.x);
+            left_Collision.position = right_Collision_Position;
+            right_Collision.position = left_Collision_Position;
+         }
+
+        transform.localScale = tempScale;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("OnCollisionEnter2D");
+        if(collision.gameObject.tag == MyTags.PLAYER_TAG) 
+        {
+            anim.Play("Stunned");
+        }
+    }
+}
---
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    Rigidbody2D playerBody;
+    Animator anim;
+
+    public Transform groundCheckPosition;
+    public LayerMask groundLayer;
+
+    bool isGrounded;
+    bool isJumped;
+    float jumpPower = 12f;
+
+    private void Awake()
+    {
+        playerBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();    
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        CheckIfGrounded();
+        PlayedJump();
+
+    }
+
+    void FixedUpdate()
+    {
+        PlayerWalk();
+    }
+
+    void PlayerWalk()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        if(x > 0)
+        {
+            playerBody.velocity = new Vector2(speed, playerBody.velocity.y);
+            changeDirection(1);
+        } else if (x < 0)
+        {
+            playerBody.velocity = new Vector2(-speed, playerBody.velocity.y);
+            changeDirection(-1);
+        } else
+        {
+            playerBody.velocity = new Vector2(0f, playerBody.velocity.y);
+        }
+
+        anim.SetInteger("Speed", Mathf.Abs((int) playerBody.velocity.x));
+    }
+
+    void changeDirection(int direction)
+    {
+        Vector3 tempscale = transform.localScale;
+        tempscale.x = direction;
+        transform.localScale = tempscale;
+    }
+
+    void CheckIfGrounded()
+    {
+        isGrounded = Physics2D.Raycast(groundCheckPosition.position, Vector2.down, 0.1f, groundLayer);
+        if(isGrounded)
+        {
+            if(isJumped)
+            {
+                isJumped = false;
+                anim.SetBool("Jump", false);
+            }
+        }
+    }
+
+    void PlayedJump()
+    {
+        if(isGrounded)
+        {
+            if(Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow))
+            {
+                isJumped = true;
+                playerBody.velocity = new Vector2(playerBody.velocity.x, jumpPower);
+                anim.SetBool("Jump", true);
+            }
+        }
+    }
+}
