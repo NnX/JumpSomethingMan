6f092d3eeb728d061d187fc22aac37e1186c0eb1
NnX <hentis@ukr.net>
2020-06-16 16:30:17 +0300

    Implement restart level

---
+++ Assets/Scripts/Controller Scripts/RestartLevel.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartLevel : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == MyTags.PLAYER_TAG)
+        {
+            SceneManager.LoadScene("GamePlay");
+        }
+    }
+}
